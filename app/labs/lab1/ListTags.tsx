8f4a829b3afc329ae5dbc90860be6db3b0aac871
MinHsun <jackson2000118@gmail.com>
2026-09-22 07:43:16 +0800

    3

---
+++ app/labs/lab1/ListTags.tsx
@@ -0,0 +1,46 @@
+export default function ListTags() {
+  return (
+    <div id="wd-lists">
+      <h4>List Tags</h4>
+
+      <h5>Ordered List Tag</h5>
+      <ol id="wd-pancakes">
+        <li>Mix dry ingredients.</li>
+        <li>Add wet ingredients.</li>
+        <li>Stir to combine.</li>
+        <li>Heat a skillet or griddle.</li>
+        <li>Pour batter onto the skillet.</li>
+        <li>Cook until bubbly on top.</li>
+        <li>Flip and cook the other side.</li>
+        <li>Serve and enjoy!</li>
+      </ol>
+
+      <h5>Unordered List Tag</h5>
+      <ul id="wd-my-books">
+        <li>Dune</li>
+        <li>Lord of the Rings</li>
+        <li>Ender&apos;s Game</li>
+        <li>Red Mars</li>
+        <li>The Forever War</li>
+      </ul>
+
+      <h5>Favorite Sports Players</h5>
+      <ol id="wd-fav-sports-players">
+        <li>Harry Kane</li>
+        <li>Paul George</li>
+        <li>Lionel Messi</li>
+        <li>Michael Olise</li>
+        <li>Anthony Edwards</li>
+      </ol>
+
+      <h5>Favorite Video Games</h5>
+      <ul id="wd-fav-video-games">
+        <li>Sekiro: Shadows Die Twice</li>
+        <li>Elden Ring</li>
+        <li>Monster Hunter: World</li>
+        <li>Stellar Blade</li>
+        <li>Pokemon Scarlet and Violet</li>
+      </ul>
+    </div>
+  );
+}
